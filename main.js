18abc4d9f71044000782f34343ce1596093fd27c
mohammad annab <eng.mannab@yahoo.com>
2019-09-19 13:50:00 +0300

    project is done

--- main.js
+++ main.js
@@ -1,30 +1,78 @@
 
-
-var powerButtonToggle = {value:false}
-var power = $('#power');
-var px = $('#welcome');
-
-
-power.on('click', function btns(button){
-	button.value = !button.value;
-	return button.value;
+var powerOn = $('#powerOn');
+var screen = $('#screen');
+// var screenText = $('#screenText')
+powerOn.on('click', function(){
+	screen.text("Welcome");
 })
 
 
-function btns(button){
-	button.value = !button.value;
-	return button.value;
-}
+var powerOff = $('#powerOff');
 
-
-
-
-power.on('click', function(){
-	px.append("the machin is on");
-	// button.value = !button.value;
-	// return button.value;
+powerOff.on('click', function(){
+	screen.text("Goodbye");
 })
 
 
+var milkButton = $('#milkButton');
+
+milkButton.on('click', function(){
+	alert("choose the quantity you want");
+})
+
+var quarter = $('#quantity1');
+
+quarter.on('click', function(){
+	alert('plese wait 3 seconds');
+	setTimeout(function(){
+		alert('here is your cup');
+	},3000)
+});
+
+var half = $('#quantity2');
+half.on('click', function(){
+	alert('plese wait 6 seconds');
+	setTimeout(function(){
+		alert('here is your cup');
+	},6000)
+});
+
+var threeQuarters = $('#quantity3');
+threeQuarters.on('click', function(){
+	alert('plese wait 9 seconds');
+	setTimeout(function(){
+		alert('here is your cup');
+	},9000)
+});
 
 
+
+var coffee = $('#coffeeButton');
+coffee.on('click', function(){
+	alert('plese wait 5 seconds');
+	setTimeout(function(){
+		alert('here is your coffee');
+	},5000)
+});
+
+
+
+
+
+	
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+	

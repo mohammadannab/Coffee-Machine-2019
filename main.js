a119b8554945176e256137f55e5c5e80d10443c6
mohammad annab <eng.mannab@yahoo.com>
2019-09-19 09:50:03 +0300

    mohammadannab

---
+++ main.js
@@ -0,0 +1,25 @@
+
+var powerButtonToggle = {value:false}
+var power = $('#power');
+var px = $('#welcome');
+
+
+power.on('click', function btns(button){
+	button.value = !button.value;
+	return button.value;
+})
+
+
+function btns(button){
+	button.value = !button.value;
+	return button.value;
+}
+
+
+
+
+power.on('click', function(){
+	px.append("the machin is on");
+	// button.value = !button.value;
+	// return button.value;
+})
